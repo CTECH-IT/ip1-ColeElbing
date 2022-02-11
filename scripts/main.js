6d94e1227acf966d832abedb096dcb8d0570251b
Cole Elbing <8517401@isd535.org>
2022-02-10 21:40:48 -0600

    Finished the game, added a win and loss feature, added a third ball and changed the background color to black

--- scripts/main.js
+++ scripts/main.js
@@ -7,13 +7,19 @@ let y = canvas.height / 2;
 let x2 = (canvas.width / 2) + 30;
 let y2 = canvas.height / 2 - 30;
 
+let x3 = (canvas.width / 2) + 30;
+let y3 = canvas.height / 2 - 30;
+
 speed = 10;
 
 let dx = 2;
 let dy = -2;
 
-let dx2 = -2;
-let dy2 = 2;
+let dx2 = -3;
+let dy2 = 3;
+
+let dx3 = -4;
+let dy3 = 4;
 
 //create lives
 let lives = 3;
@@ -47,6 +53,15 @@ function drawBall2() {
     ctx.closePath();
 }
 
+//third ball
+function drawBall3() {
+    ctx.beginPath();
+    ctx.arc(x3, y3, ballSize, 0, Math.PI * 2);
+    ctx.fillStyle = "#FFF01F";
+    ctx.fill();
+    ctx.closePath();
+}
+
 //Creates the movable character
 function drawCharacter() {
     ctx.beginPath();
@@ -70,6 +85,23 @@ function drawLives() {
     ctx.fillText("Lives: " + lives, canvas.width - 60, 20);
 }
 
+function level2() {
+    if (score >= 200) {
+        drawBall2();
+        collisionDetection2();
+        collisionDetection3();
+    }
+}
+
+function level3(){
+    if(score >=500){
+        drawBall3();
+        collisionDetection4();
+        collisionDetection5();
+        collisionDetection6();
+    }
+}
+
 //Drawing all of the objects
 function draw() {
     //clear the canvas
@@ -91,9 +123,11 @@ function draw() {
     collisionDetection();
 
     //Creates more balls
-    if(score >= 200){
-        drawBall2();
-    }
+    level2();
+    level3();
+
+    //if score reaches 1000 the player wins
+    win();
 
     //Change the x and y values of the ball
     x += dx;
@@ -102,6 +136,10 @@ function draw() {
     //Change the x and y values of the second ball
     x2 += dx2;
     y2 += dy2;
+
+    //Chnages x and y values of third ball
+    x3 += dx3;
+    y3 += dy3;
 
     //check if the initial ball has hit the sides.
     if (x + dx > canvas.width - ballSize || x + dx < ballSize) {
@@ -119,54 +157,119 @@ function draw() {
         dy2 = -dy2;
     }
 
+    //checks if the third ball hits the sides
+    if (x3 + dx3 > canvas.width - ballSize || x3 + dx3 < ballSize) {
+        dx3 = -dx3;
+    }
+    if (y3 + dy3 > canvas.height - ballSize || y3 + dy3 < ballSize) {
+        dy3 = -dy3;
+    }
+
     //character controls
     if (rightPressed) {
         charX += 3;
-        if(charX + charSize > canvas.width){
+        if (charX + charSize > canvas.width) {
             charX = canvas.width - charSize;
         }
     }
-    else if (leftPressed) {
+    if (leftPressed) {
         charX -= 3;
-        if (charX < 0){
-            charX = 0 + 2*charSize;
+        if (charX < 0) {
+            charX = 0 + 2 * charSize;
         }
     }
-    else if (upPressed){
+    if (upPressed) {
         charY -= 3;
-        if (charY < 0){
-            charY = 0 + 2*charSize;
+        if (charY < 0) {
+            charY = 0 + 2 * charSize;
         }
     }
-    else if (downPressed){
+    if (downPressed) {
         charY += 3;
-        if(charY + charSize > canvas.height){
+        if (charY + charSize > canvas.height) {
             charY = canvas.height - charSize;
         }
     }
-    
 }
-function collisionDetection(){
-    if (Math.sqrt(Math.pow((x-charX),2) + Math.pow((y-charY),2)) < charSize + ballSize){
+// detects the collision of character ball and the initial ball
+function collisionDetection() {
+    if (Math.sqrt(Math.pow((x - charX), 2) + Math.pow((y - charY), 2)) < charSize + ballSize) {
+        dy = -dy;
+        dx = -dx;
+        lives--;
+    }
+}
+
+//detects the collision of character ball and second ball
+function collisionDetection2() {
+    if (Math.sqrt(Math.pow((x2 - charX), 2) + Math.pow((y2 - charY), 2)) < charSize + ballSize) {
+        dy2 = -dy2;
+        dx2 = -dx2;
+        lives--;
+
+    }
+}
+
+//detects collisionn bewteen initial ball and second ball
+function collisionDetection3() {
+    if (Math.sqrt(Math.pow((x2 - x), 2) + Math.pow((y2 - y), 2)) < ballSize + ballSize) {
+        dy2 = -dy2;
+        dx2 = -dx2;
+        dx = -dx;
+        dy = -dy;
+    }
+}
+
+function collisionDetection4() {
+    if (Math.sqrt(Math.pow((x3 - charX), 2) + Math.pow((y3 - charY), 2)) < charSize + ballSize) {
+        dy3 = -dy3;
+        dx3 = -dx3;
+        lives--;
+    }
+}
+
+function collisionDetection5(){
+    if (Math.sqrt(Math.pow((x3 - x), 2) + Math.pow((y3 - y), 2)) < ballSize * 2) {
+        dy3 = -dy3;
+        dx3 = -dx3;
         dy = -dy;
         dx = -dx;
     }
 }
 
-// Score counter based on lives
-function counter() {
-    let n = 4
-    if (lives > 0) {
-        score = score + n;
-        if (lives = lives--) {
-            n -= n;
-        }
-    }
-    else if(score >= 200){
-        drawBall();
+function collisionDetection6(){
+    if (Math.sqrt(Math.pow((x3 - x2), 2) + Math.pow((y3 - y2), 2)) < ballSize * 2) {
+        dy3 = -dy3;
+        dx3 = -dx3;
+        dy2 = -dy2;
+        dx2 = -dx2;
     }
 }
 
+
+
+// Score counter based on lives
+function counter() {
+    let n = 5
+    if (lives > -1) {
+        score = score + n;
+    }
+    else{
+        alert("GAME OVER");
+        document.location.reload();
+        clearInterval(interval);
+        clearInterval(increase);
+    }
+}
+
+function win(){
+    if(score > 1000){
+        alert("CONGRATS YOU WIN");
+        document.location.reload();
+        clearInterval(interval);
+        clearInterval(increase);
+    }
+}
 //tells when the keys are being pressed
 function keyDownHandler(e) {
     if (e.key == 'Right' || e.key == "ArrowRight") {
@@ -182,7 +285,7 @@ function keyDownHandler(e) {
         downPressed = true;
     }
 
-//tells when the keys are NOT being pressed
+    //tells when the keys are NOT being pressed
 }
 function keyUpHandler(e) {
     if (e.key == 'Right' || e.key == "ArrowRight") {
@@ -203,5 +306,5 @@ function keyUpHandler(e) {
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
-let increase = setInterval(counter, 1000);
+let increase = setInterval(counter, 275);
 let interval = setInterval(draw, speed);
